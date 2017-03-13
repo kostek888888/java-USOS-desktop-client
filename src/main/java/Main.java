c30049da422bfd90234e5ce70ec60ff4574b5c70
Tomasz Kowalczyk <tomasz.kowalczyk94@gmail.com>
2017-03-13 16:16:45 +0100

    parse html example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("troloo xxxxxxx");
-
+		
+		Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+		Elements newsHeadlines = doc.select("#mp-itn b a");
+		System.out.println(newsHeadlines.text());
+		
+		
 	}
 
 }
