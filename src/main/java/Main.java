8d80edd3b139ee137f2eb9b3d2d0d7ce8631ad0c
Tomasz Kowalczyk <tomasz.kowalczyk94@gmail.com>
2017-04-22 11:29:55 +0200

    database example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 
 import org.jsoup.Connection.Method;
@@ -13,9 +18,33 @@ import usos.UsosManager;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLException {
 		
-		LoginWindow loginObj = new LoginWindow();
+		//LoginWindow loginObj = new LoginWindow();
+		
+		
+		Connection conn;
+		
+		String dbUrl = "jdbc:derby:database;create=true";
+	    conn = DriverManager.getConnection(dbUrl);
+		
+	    Statement stmt = conn.createStatement();
+	    stmt.executeUpdate("Create table users (id int primary key, name varchar(30))");
+		
+	    // insert 2 rows
+	    stmt.executeUpdate("insert into users values (1,'tom')");
+	    stmt.executeUpdate("insert into users values (2,'peter')");
+	 
+	    // query
+	    ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+		 
+	    while (rs.next()) { 
+	        System.out.printf("%d\t%s\n", rs.getInt("id"), rs.getString("name"));
+	    }
+		
+		
+		
+		
 		
 		
 		/*
