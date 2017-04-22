ae71ea60471bf8111e52097169dd15a62a81f5e5
wojtek <wojtek.klusek2@gmail.com>
2017-04-22 21:06:35 +0200

    Panel logowania

--- src/main/java/gui/guiController.java
+++ src/main/java/gui/guiController.java
@@ -1,5 +1,24 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class guiController {
-	
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField loginTextField;
+
+    @FXML
+    private TextField passTextField;
+
+    @FXML
+    void loginButtonClick(MouseEvent event) {
+    	loginTextField.setText("KEK");
+    }
+
 }
