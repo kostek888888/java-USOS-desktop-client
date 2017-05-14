bb2f62c6794b93756a5bb2097bcb3c63e5972a47
wojtek <wojtek.klusek2@gmail.com>
2017-05-14 20:37:15 +0200

    Dodanie language do klasy UsosManager

--- src/main/java/usos/UsosManager.java
+++ src/main/java/usos/UsosManager.java
@@ -7,8 +7,21 @@ import usos.database.Element;
 import usos.helper.Semester;
 
 public class UsosManager {
+	{
+		setLanguage("english");
+	}
+	
+	private String language;
 	
 	
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
 	protected UsosLoginManager usosLoginManager;
 	protected UsosMarksManager usosMarksManager;
 	protected DatabaseManager databaseManager;
