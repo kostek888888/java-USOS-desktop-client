0fd574d1ecf5575685bd4d65ee2af7fc206045b5
wojtek <wojtek.klusek2@gmail.com>
2017-05-15 20:37:39 +0200

    Revert "ś"
    This reverts commit 76755bf097c4d9d2ed6b6006645783fe1e4859f2.

--- src/main/java/gui/HomeController.java
+++ src/main/java/gui/HomeController.java
@@ -1,39 +1,15 @@
 package gui;
 
-import java.io.File;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public class HomeController {
-	
-	String soundPath = "src/main/resources/sound.mp3";
 
     @FXML
     private AnchorPane anchorPane;
 
     @FXML
     private Label loginLabel;
-    
-    @FXML
-    private Button testDzwiekuButton;
-
-    @FXML
-    void testDzwiekuClick(MouseEvent event) {
-    	try{
-    	 	Media sound = new Media(new File(soundPath).toURI().toString());
-        	MediaPlayer mediaPlayer = new MediaPlayer(sound);
-        	mediaPlayer.play();
-    	} catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("Nie znaleziono pliku");
-    	}
-    }
-   
 
 }
