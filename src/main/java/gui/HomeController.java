bff85c26e44071994e41b8d836b58c271810158d
wojtek <wojtek.klusek2@gmail.com>
2017-05-15 21:33:30 +0200

    Test dzwieku 2

--- src/main/java/gui/HomeController.java
+++ src/main/java/gui/HomeController.java
@@ -1,10 +1,15 @@
 package gui;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
+import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
@@ -22,13 +27,36 @@ public class HomeController {
     
     @FXML
     private Button testDzwiekuButton;
+    
+    private TableView<?> tableView;
+    
+    @FXML
+    private TableColumn<?, ?> columnSubject;
 
+    @FXML
+    private TableColumn<?, ?> columnType;
+
+    @FXML
+    private TableColumn<?, ?> columnDate1;
+
+    @FXML
+    private TableColumn<?, ?> columnDate2;
+
+    @FXML
+    private TableColumn<?, ?> columnDate3;
+    
+    
+    
     @FXML
     void testDzwiekuClick(MouseEvent event) {
     	try{
     	 	Media sound = new Media(new File(soundPath).toURI().toString());
         	MediaPlayer mediaPlayer = new MediaPlayer(sound);
         	mediaPlayer.play();
+        	testDzwiekuButton.setDisable(true);
+        	
+      
+        	
     	} catch(Exception e) {
 			e.printStackTrace();
 			System.out.println("Nie znaleziono pliku");
