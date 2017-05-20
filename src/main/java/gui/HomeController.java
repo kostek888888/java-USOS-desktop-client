1dde3c4b104ba5248f894154e09b57f7f6283a55
Tomasz Kowalczyk <tomasz.kowalczyk94@gmail.com>
2017-05-20 13:08:06 +0200

    dla wojtka pobieranie ocen

--- src/main/java/gui/HomeController.java
+++ src/main/java/gui/HomeController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,6 +15,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import usos.UsosManager;
+import usos.helper.Semester;
 
 public class HomeController {
 	
@@ -45,21 +48,34 @@ public class HomeController {
     @FXML
     private TableColumn<?, ?> columnDate3;
     
+    private UsosStage getUsosStage() {
+    	//FUSZERKA !!! @todo
+    	return  (UsosStage) anchorPane.getScene().getWindow();
+    }
+    
+    private UsosManager getUsosManager() {
+    	//FUSZERKA !!! @todo
+        return getUsosStage().getUsosManager();
+    }
     
     
     @FXML
-    void testDzwiekuClick(MouseEvent event) {
+    void testDzwiekuClick(MouseEvent event) throws IOException {
     	try{
     	 	Media sound = new Media(new File(soundPath).toURI().toString());
         	MediaPlayer mediaPlayer = new MediaPlayer(sound);
         	mediaPlayer.play();
         	testDzwiekuButton.setDisable(true);
         	
-      
+        	//Semester semester = getUsosManager().getMarksForLastSemester();
+			//System.out.println(semester.getName());
         	
     	} catch(Exception e) {
 			e.printStackTrace();
 			System.out.println("Nie znaleziono pliku");
+			
+			
+			
     	}
     }
    
